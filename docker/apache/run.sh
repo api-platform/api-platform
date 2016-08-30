b39e47db93c893768c6b060293fc6abaa8aa4015
Kévin Dunglas <dunglas@gmail.com>
2016-08-30 07:55:51 +0200

    Detect bridge IP

--- docker/apache/run.sh
+++ docker/apache/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -xe
 
+# Detect the host IP
+export DOCKER_BRIDGE_IP=$(ip ro | grep default | cut -d' ' -f 3)
+
 # Start Apache with the right permissions
 /app/docker/apache/start_safe_perms -DFOREGROUND
