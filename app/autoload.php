a646b2ef88a53ebcf0a1a09dd98a6ddcbb3c9d71
Teoh Han Hui <teohhanhui@gmail.com>
2016-09-27 19:31:15 +0800

    Change inline var PHPDoc syntax

--- app/autoload.php
+++ app/autoload.php
@@ -3,9 +3,7 @@
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-/**
- * @var ClassLoader
- */
+/** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
--- web/app.php
+++ web/app.php
@@ -2,9 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-/*
- * @var Composer\Autoload\ClassLoader
- */
+/** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
