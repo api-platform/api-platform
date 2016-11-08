fc9ba635693c8bbdd4561b290299ba8a1d4d0359
Kévin Dunglas <dunglas@gmail.com>
2016-11-08 12:03:00 +0100

    Simplify composer.sh

--- docker/composer.sh
+++ docker/composer.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-php -r "copy('https://composer.github.io/installer.sig', 'installer.sig');"
-EXPECTED_SIGNATURE=$(php -r "echo trim(file_get_contents('installer.sig'));")
+EXPECTED_SIGNATURE=$(php -r "echo trim(file_get_contents('https://composer.github.io/installer.sig'));")
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
