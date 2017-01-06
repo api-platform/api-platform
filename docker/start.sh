2bed311df5250262e452a585bbf5700489b00a18
Gildas NOEL <g.noel@ackwa.fr>
2017-01-06 08:01:25 +0100

    Remove pre-existing PID file before starting apache

--- docker/start.sh
+++ docker/start.sh
@@ -10,5 +10,6 @@ else
     composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader
 fi
 
-# Start Apache with the right permissions
+# Start Apache with the right permissions after removing pre-existing PID file
+rm -f /var/run/apache2/apache2.pid
 exec docker/apache/start_safe_perms -DFOREGROUND
