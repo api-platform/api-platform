17c9efaeae4fe26fc8eb4de1f622b2402a775c3c
Kévin Dunglas <dunglas@gmail.com>
2017-01-26 18:07:21 +0100

    Always use the dev env if the SYMFONY_ENV var isn't set

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,10 @@ set -xe
 # Detect the host IP
 export DOCKER_BRIDGE_IP=$(ip ro | grep default | cut -d' ' -f 3)
 
-if [ "$SYMFONY_ENV" = 'dev' ]; then
-    composer install --prefer-dist --no-progress --no-suggest
-else
+if [ "$SYMFONY_ENV" = 'prod' ]; then
     composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader --classmap-authoritative
+else
+    composer install --prefer-dist --no-progress --no-suggest
 fi
 
 # Start Apache with the right permissions after removing pre-existing PID file
