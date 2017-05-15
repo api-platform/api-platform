83551bdbc2279a41854e8a7b8113700fba90b73c
Kévin Dunglas <dunglas@gmail.com>
2017-05-15 15:44:14 +0200

    Bump dependencies

--- web/config.php
+++ web/config.php
@@ -11,7 +11,7 @@
  */
 
 if (!isset($_SERVER['HTTP_HOST'])) {
-    exit('This script cannot be run from the CLI. Run it from a browser.');
+    exit("This script cannot be run from the CLI. Run it from a browser.\n");
 }
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
