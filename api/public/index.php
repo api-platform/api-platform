a59d6a3609180cb9a46bb652d2cdad281e39d9d2
Kévin Dunglas <dunglas@gmail.com>
2018-01-20 00:04:44 +0100

    Fix trusted proxies.

--- api/public/index.php
+++ api/public/index.php
@@ -25,7 +25,7 @@ if ($debug) {
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
