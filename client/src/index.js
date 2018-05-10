0f0979c4232c37352e6fa9ff4119dc89ee3b7fd2
Dylan Ballandras <dylan.ballandras@gmail.com>
2018-05-10 23:16:04 +0200

    update of react-redux-router dependency

--- client/src/index.js
+++ client/src/index.js
@@ -4,31 +4,33 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reducer as form } from 'redux-form';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-import { syncHistoryWithStore, routerReducer as routing } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware, routerReducer as routing } from 'react-router-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import registerServiceWorker from './registerServiceWorker';
 // Import your reducers and routes here
 import Welcome from './Welcome';
 
+const history = createBrowserHistory();
+const historyMiddleware = routerMiddleware(history);
+
 const store = createStore(
   combineReducers({routing, form, /* Add your reducers here */}),
   applyMiddleware(thunk),
+  applyMiddleware(historyMiddleware),
 );
-
-const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 ReactDom.render(
   <Provider store={store}>
-    <Router history={history}>
+    <ConnectedRouter history={history}>
       <Switch>
         <Route path="/" component={Welcome} strict={true} exact={true}/>
         {/* Add your routes here */}
         <Route render={() => <h1>Not Found</h1>}/>
       </Switch>
-    </Router>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
