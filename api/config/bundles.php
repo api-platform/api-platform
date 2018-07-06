1bf62a3d37b08795ddf2b21ea849ebe1808fb133
Kévin Dunglas <dunglas@gmail.com>
2018-07-06 15:51:03 +0200

    Bump dependencies

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
