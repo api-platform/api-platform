74f8e13c7f1ac7f5c74887cc5eefaedff40e6d82
Fabious <fabious@free.fr>
2018-10-01 15:25:47 +0200

    migrate admin create-react-app to v2

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.unregister();
