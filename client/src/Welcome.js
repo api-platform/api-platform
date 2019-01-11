ece177487a393f382e88922ae8405dbe5c775725
Kévin Dunglas <dunglas@gmail.com>
2019-01-11 10:40:23 +0100

    Remove useless parameter

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -129,7 +129,7 @@ const HelpButton = ({ Image, url, title }) => (
 
 export default Welcome;
 
-const Logo = ({ className }) => (
+const Logo = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width={150}
