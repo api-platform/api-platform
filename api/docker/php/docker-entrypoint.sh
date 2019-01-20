90b7b8e8b5979a15eb2cdc8f85858b0cb72f0d01
Alan Poulain <contact@alanpoulain.eu>
2019-01-20 02:11:29 +0100

    Fix entrypoint for MongoDB

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -21,13 +21,15 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 	fi
 
-	echo "Waiting for db to be ready..."
-	until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-		sleep 1
-	done
+	if [ "$MONGODB" != 1 ]; then
+		echo "Waiting for db to be ready..."
+		until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+			sleep 1
+		done
 
-	if [ "$APP_ENV" != 'prod' ]; then
-		bin/console doctrine:schema:update --force --no-interaction
+		if [ "$APP_ENV" != 'prod' ]; then
+			bin/console doctrine:schema:update --force --no-interaction
+		fi
 	fi
 fi
 
