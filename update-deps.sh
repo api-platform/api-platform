e389969c09e644c74f3bb8a698d26624774f11e1
Lctrs <jerome@prmntr.me>
2019-05-03 13:32:40 +0200

    update-deps.sh: Run composer sync-recipes inside php container

--- update-deps.sh
+++ update-deps.sh
@@ -2,8 +2,7 @@
 
 docker-compose pull
 docker-compose build
-docker-compose run php composer update
+docker-compose run php /bin/sh -c 'composer update && composer sync-recipes --force'
 docker-compose run admin /bin/sh -c 'yarn install && yarn upgrade'
 docker-compose run client /bin/sh -c 'yarn install && yarn upgrade'
-cd api && composer sync-recipes --force
 echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
