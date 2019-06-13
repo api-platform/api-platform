f7840fc1e2ad50bedc72075ec0fca1ea27e432ff
Kévin Dunglas <dunglas@gmail.com>
2019-06-13 08:44:09 +0200

    Add MakerBundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
