5791b0cf8467e9bc0922dbe4b4126f2cdc5dc5f7
Philippe Gerber <philippe@bigwhoop.ch>
2019-06-14 16:32:06 +0200

    \Symfony\Component\Dotenv\Dotenv has no constructor that could take arguments

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -12,7 +12,7 @@ if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
     // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
 
 $_SERVER += $_ENV;
