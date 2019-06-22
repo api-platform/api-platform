6a2ff40552d7d7b008c698cd120eea7f01fb1247
Kévin Dunglas <dunglas@gmail.com>
2019-06-22 13:03:19 +0200

    Bump deps

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -7,7 +7,9 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
-    $_ENV += $env;
+    foreach ($env as $k => $v) {
+        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
+    }
 } elseif (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
