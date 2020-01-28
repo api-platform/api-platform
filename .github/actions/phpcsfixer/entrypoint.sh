1e66c40a03eff09f7702dfc636fbb86f780dee54
Dani Lopez Garcia <dani16lp@gmail.com>
2020-01-28 19:38:25 +0100

    Update entrypoint.sh

--- .github/actions/phpcsfixer/entrypoint.sh
+++ .github/actions/phpcsfixer/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-/php-cs-fixer fix src/ --verbose --rules=@Symfony,-no_superfluous_phpdoc_tags,-no_unused_imports
+/php-cs-fixer fix api/src/ --verbose --rules=@Symfony,-no_superfluous_phpdoc_tags,-no_unused_imports
