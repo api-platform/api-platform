43132866cb000b921a7ef1c782f2fdff07db097f
Kévin Dunglas <dunglas@gmail.com>
2020-02-25 15:16:41 +0100

    Update client index

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,14 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reducer as form } from 'redux-form';
 import { Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import {
   ConnectedRouter,
   connectRouter,
   routerMiddleware
 } from 'connected-react-router';
 import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import * as serviceWorker from './serviceWorker';
 // Import your reducers and routes here
 import Welcome from './Welcome';
