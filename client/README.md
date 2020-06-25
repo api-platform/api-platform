1e26a37467a6eeb04edea26d738ddfe8a7007306
thcolin <me@thcol.in>
2020-06-25 14:22:53 +0200

    Add components folder

--- client/README.md
+++ client/README.md
@@ -5,8 +5,6 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 First, run the development server:
 
 ```bash
-npm run dev
-# or
 yarn dev
 ```
 
---
+++ client/components/HelpButton/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelpButton = ({ Image, url, title }) => (
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href={url}
+    className="help__circle"
+    title={title}
+  >
+    <Image />
+  </a>
+);
+
+export default HelpButton;
