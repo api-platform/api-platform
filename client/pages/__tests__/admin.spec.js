d7cf84c6a7266c7b70e6c39304fe358ae9ef2f78
Alan Poulain <contact@alanpoulain.eu>
2020-10-16 17:48:08 +0200

    fix: remove page tests and fix build serve

--- client/pages/__tests__/admin.spec.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Admin from "../admin";
-
-test("renders admin page", () => {
-  const { baseElement } = render(<Admin />);
-  expect(baseElement).toBeTruthy();
-});
--- client/pages/__tests__/home.spec.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "../index";
-
-test("renders home page", () => {
-  const { baseElement } = render(<Home />);
-  expect(baseElement).toBeTruthy();
-});
