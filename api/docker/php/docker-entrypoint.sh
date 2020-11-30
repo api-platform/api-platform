155460eb1c686bce354dd5b60b93f3245b59e13e
Kévin Dunglas <dunglas@gmail.com>
2020-12-01 00:24:50 +0100

    feat: use PHP 8

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
 	if [ "$APP_ENV" != 'prod' ]; then
-		composer install --prefer-dist --no-progress --no-suggest --no-interaction
+		composer install --ignore-platform-req=php --prefer-dist --no-progress --no-interaction
 	fi
 
 	echo "Waiting for db to be ready..."
