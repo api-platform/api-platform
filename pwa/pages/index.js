ee2f54170d38d3c33d93c73d5f594b8c5ced429d
Kévin Dunglas <dunglas@gmail.com>
2021-01-04 00:10:14 +0100

    fix: Link must contain an anchor

--- pwa/pages/index.js
+++ pwa/pages/index.js
@@ -39,7 +39,8 @@ const Welcome = () => (
               <a href="https://nextjs.org/">
                 <b>Next.js</b>
               </a>{" "}
-              application. Learn how to create your first API and generate a PWA:
+              application. Learn how to create your first API and generate a
+              PWA:
             </p>
             <a
               target="_blank"
@@ -59,7 +60,7 @@ const Welcome = () => (
               </div>
               <div className="other__content">
                 <h3>
-                  <Link href="/docs">API</Link>
+                  <a href="/docs">API</a>
                 </h3>
               </div>
             </div>
@@ -69,7 +70,9 @@ const Welcome = () => (
               </div>
               <div className="other__content">
                 <h3>
-                  <Link href="/admin">Admin</Link>
+                  <Link href="/admin">
+                    <a>Admin</a>
+                  </Link>
                 </h3>
               </div>
             </div>
@@ -1951,7 +1954,7 @@ const WebbyWelcome = () => (
     width="400"
     height="400"
     viewBox="0 0 300 300"
-    >
+  >
     <style jsx>
       {`
         .webbyWelcome {
