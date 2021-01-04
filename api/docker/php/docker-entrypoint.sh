af2492b220ef468816a0558e497a0945bc67f8c0
Kévin Dunglas <dunglas@gmail.com>
2021-01-04 20:20:42 +0100

    feat: allow to use the Docker setup without DB

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -21,23 +21,25 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
-	echo "Waiting for db to be ready..."
-	ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-	until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-		sleep 1
-		ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE-1))
-		echo "Still waiting for db to be ready... Or maybe the db is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left"
-	done
+	if grep -q DATABASE_URL= .env; then
+		echo "Waiting for database to be ready..."
+		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
+			sleep 1
+			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left"
+		done
 
-	if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-		echo "The db is not up or not reachable"
-		exit 1
-	else
-	   echo "The db is now ready and reachable"
-	fi
+		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+			echo "The database is not up or not reachable"
+			exit 1
+		else
+			echo "The database is now ready and reachable"
+		fi
 
-	if ls -A migrations/*.php > /dev/null 2>&1; then
-		bin/console doctrine:migrations:migrate --no-interaction
+		if ls -A migrations/*.php >/dev/null 2>&1; then
+			bin/console doctrine:migrations:migrate --no-interaction
+		fi
 	fi
 fi
 
