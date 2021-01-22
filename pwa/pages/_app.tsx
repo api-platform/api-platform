d0d8c2b9bd3bb1e2cce1181d9b17e7628997d1d8
Kévin Dunglas <dunglas@gmail.com>
2021-01-22 23:42:41 +0100

    feat: make changes necessary for the latest version of Client Generator  (#1775)
    * feat: upgrade to the last version of Client Generator
    
    * feat: load bootstrap
    
    * fix: rename index.js in index.tsx
    
    * feat: add bootstrap-icons

---
+++ pwa/pages/_app.tsx
@@ -0,0 +1,22 @@
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default MyApp;
