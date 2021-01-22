d9c89e2837884248620943ba381172c358d44beb
Kévin Dunglas <dunglas@gmail.com>
2021-01-22 23:24:10 +0100

    feat: add bootstrap-icons

--- pwa/pages/_app.tsx
+++ pwa/pages/_app.tsx
@@ -1,6 +1,7 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from "next/app";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
