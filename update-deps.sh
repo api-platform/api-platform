2d3c2ff66de948532df6a14f18b18f06665e336f
Kévin Dunglas <dunglas@gmail.com>
2021-04-12 17:19:35 +0200

    Bump deps

--- update-deps.sh
+++ update-deps.sh
@@ -15,7 +15,7 @@ composer sync-recipes --force
 # Hint the user to change APP_SECRET
 sed -i.bak 's/^APP_SECRET=.*$/APP_SECRET=!ChangeMe!/' .env
 # Compatibility with our Docker and Kubernetes setup
-sed -i.bak 's;^DATABASE_URL="postgresql://db_user:db_password@127.0.0.1:5432/db_name?serverVersion=13&charset=utf8"$;DATABASE_URL="postgresql://api-platform:!ChangeMe!@database:5432/api?serverVersion=13&charset=utf8";' .env
+sed -i.bak 's;^DATABASE_URL="postgresql://db_user:db_password@127.0.0.1:5432/db_name?serverVersion=13\&charset=utf8"$;DATABASE_URL="postgresql://api-platform:!ChangeMe!@database:5432/api?serverVersion=13&charset=utf8";' .env
 sed -i.bak 's;^MERCURE_PUBLISH_URL=http://mercure/.well-known/mercure$;MERCURE_PUBLISH_URL=http://caddy/.well-known/mercure;' .env
 
 rm .env.bak
