42122f4179a73f4398f829753cea97e520e068f3
Vincent Chalamon <vincentchalamon@protonmail.com>
2021-04-20 18:55:37 +0200

    chore: update helm chart and documentation, and fix some bugs and typos

--- pwa/config/entrypoint.ts
+++ pwa/config/entrypoint.ts
@@ -1,1 +1,1 @@
-export const ENTRYPOINT = typeof window === "undefined" ? "http://caddy" : window.origin;
+export const NEXT_PUBLIC_ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_ENTRYPOINT : window.origin;
