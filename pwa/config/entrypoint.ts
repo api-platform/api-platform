afae1579b389d1d4257d53c5fbc4f09a3e593361
Alan Poulain <contact@alanpoulain.eu>
2021-06-16 20:00:09 +0200

    fix: export ENTRYPOINT in pwa/config/entrypoint.ts

--- pwa/config/entrypoint.ts
+++ pwa/config/entrypoint.ts
@@ -1,1 +1,1 @@
-export const NEXT_PUBLIC_ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_ENTRYPOINT : window.origin;
+export const ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_ENTRYPOINT : window.origin;
