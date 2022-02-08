09f12a76fcaa2896dcec8ec1b56ef9e36adf69aa
Roger Saner <roger@howmightwe.co.za>
2022-02-08 12:39:56 +0200

    fix: migration can now use with postgres or mysql

--- api/migrations/Version20210930074739.php
+++ api/migrations/Version20210930074739.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -19,16 +21,24 @@ final class Version20210930074739 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE greeting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE greeting (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $platform = $this->connection->getDatabasePlatform();
+        if ($platform instanceof MySQLPlatform) {
+            $this->addSql('CREATE TABLE greeting (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        } else if ($platform instanceof PostgreSQLPlatform) {
+            $this->addSql('CREATE SEQUENCE greeting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+            $this->addSql('CREATE TABLE greeting (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        }
+
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE greeting_id_seq CASCADE');
-        $this->addSql('DROP TABLE greeting');
+        $platform = $this->connection->getDatabasePlatform();
+        if ($platform instanceof MySQLPlatform) {
+            $this->addSql('DROP TABLE greeting');
+        }  else if ($platform instanceof PostgreSQLPlatform) {
+            $this->addSql('CREATE SCHEMA public');
+            $this->addSql('DROP SEQUENCE greeting_id_seq CASCADE');
+        }
     }
 }
