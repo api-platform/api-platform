033d3d4c7709689e60b465ec4f1877a81a27553d
Vincent Chalamon <vincentchalamon@protonmail.com>
2022-07-20 16:56:21 +0200

    chore: upgrade Docker Compose commands to v2

--- update-deps.sh
+++ update-deps.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Update Docker images
-docker-compose pull
-docker-compose build
+docker compose pull
+docker compose build
 
 # Update deps
-docker-compose run php composer update
-docker-compose run pwa /bin/sh -c 'yarn install && yarn upgrade'
+docker compose run php composer update
+docker compose run pwa /bin/sh -c 'yarn install && yarn upgrade'
 
 # Update the Symfony skeleton
 cd api
 composer sync-recipes --force
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
-echo 'Run `docker-compose up --build --force-recreate` now and check that everything is fine!'
+echo 'Run `docker compose up --build --force-recreate` now and check that everything is fine!'
