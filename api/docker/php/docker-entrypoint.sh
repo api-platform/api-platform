a30df23e26eb0603ceb4402a6190680248c9332d
Kévin Dunglas <kevin@dunglas.fr>
2022-08-04 15:01:03 +0200

    feat: add native Xdebug support (#2226)
    * feat: add native Xdebug support
    
    * feat: various Docker-related improvements
    
    * feat: uniformize paths with Symfony Docker

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -7,11 +7,6 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	if [ "$APP_ENV" != 'prod' ]; then
-		ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
-	fi
-
-	mkdir -p var/cache var/log
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
