62dcf961fe459b5ec17f0ac6fc1f040ca5c86bd7
Kévin Dunglas <kevin@dunglas.fr>
2022-09-14 17:19:48 +0200

    fix: react-query installation

--- pwa/pages/_app.tsx
+++ pwa/pages/_app.tsx
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider, useQuery } from "react-query"
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
 }
 
 export default MyApp
