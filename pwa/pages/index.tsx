0e6e3372d323497e160a270c32081b6415958111
Kévin Dunglas <kevin@dunglas.fr>
2022-12-21 11:41:53 +0100

    feat: upgrade to Next 13

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -195,14 +195,14 @@ const HelpButton = ({
   title: string;
   children: React.ReactNode;
 }) => (
-  <Link href={url}>
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      className="w-12 h-12 p-2.5 rounded-full border-2 border-gray-100 justify-center transition-colors hover:border-cyan-200 hover:bg-cyan-200/50 m-2 inline-flex items-center | md:p-1 md:w-9 md:h-9 md:flex md:mx-auto md:m-0"
-      title={title}
-    >
-      {children}
-    </a>
-  </Link>
+  (<Link
+    href={url}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="w-12 h-12 p-2.5 rounded-full border-2 border-gray-100 justify-center transition-colors hover:border-cyan-200 hover:bg-cyan-200/50 m-2 inline-flex items-center | md:p-1 md:w-9 md:h-9 md:flex md:mx-auto md:m-0"
+    title={title}>
+
+    {children}
+
+  </Link>)
 );
