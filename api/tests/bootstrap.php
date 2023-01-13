bdfe4d768cc5012221e073f92363a4a2b97060cb
ole <ole@pixum.com>
2023-01-13 01:20:27 +0100

    chore: setup project

--- api/tests/bootstrap.php
+++ api/tests/bootstrap.php
@@ -9,3 +9,7 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
