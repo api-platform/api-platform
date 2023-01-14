5dc6bb538d8eec2f798c756dac180fbde4cbfaba
Kévin Dunglas <kevin@dunglas.fr>
2023-01-14 13:01:07 +0100

    chore: bump deps (#2364)

--- update-deps.sh
+++ update-deps.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+# Remove all running containers and caches
+docker compose down -v
+docker system prune -a --volumes
+
 # Update Docker images
-docker compose pull
-docker compose build
+docker compose pull --include-deps
+docker compose build --no-cache
 
 # Update deps
 docker compose run php composer update
 docker compose run pwa /bin/sh -c 'pnpm install; pnpm update'
 
-# Update the Symfony skeleton
+# Update Symfony recipes
 cd api
-composer sync-recipes --force
+composer recipes:update
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
-echo 'Run `docker compose up --build --force-recreate` now and check that everything is fine!'
+echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
