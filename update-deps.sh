112becfedece9e54a4ff75239cb26c10077e3b67
Tim Strijdhorst <tim@luscii.com>
2024-02-15 15:00:08 +0100

    fix: use docker composer instead of trying to use the host version

--- update-deps.sh
+++ update-deps.sh
@@ -11,8 +11,7 @@ docker compose run php /bin/sh -c 'composer update; composer outdated'
 docker compose run pwa /bin/sh -c 'pnpm install; pnpm update; pnpm outdated'
 
 # Update Symfony recipes
-cd api
-composer recipes:update
+docker compose run php /bin/sh -c 'cd api && composer recipes:update'
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
 echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
