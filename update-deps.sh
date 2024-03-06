572dd792f341d656c98afa1e03e68ae5c886eeb0
Kévin Dunglas <kevin@dunglas.fr>
2024-03-06 11:39:49 +0100

    chore: run tests as part of update-deps.sh

--- update-deps.sh
+++ update-deps.sh
@@ -14,5 +14,8 @@ docker compose run pwa /bin/sh -c 'pnpm install; pnpm update; pnpm outdated'
 cd api
 composer recipes:update
 
+# Run tests
+docker compose run /bin/sh -c 'bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate'
+
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
 echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
