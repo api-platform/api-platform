73fa9e8f605122e0efaf188972554c0c3809649e
Kévin Dunglas <kevin@dunglas.fr>
2024-03-07 13:37:56 +0100

    chore: bump deps

--- update-deps.sh
+++ update-deps.sh
@@ -15,7 +15,7 @@ cd api
 composer recipes:update
 
 # Run tests
-docker compose run /bin/sh -c 'bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate'
+docker compose run php /bin/sh -c 'bin/console -e test doctrine:database:create ; bin/console -e test doctrine:migrations:migrate --no-interaction ; bin/phpunit ; bin/console -e test doctrine:schema:validate'
 
 echo 'Run `git diff` and carefully inspect the changes made by the recipes.'
 echo 'Run `docker compose up --force-recreate` now and check that everything is fine!'
