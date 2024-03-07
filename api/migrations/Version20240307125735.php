271d63c5fe92350be0a165c5b5f0a3fa419059a9
Kévin Dunglas <kevin@dunglas.fr>
2024-03-07 14:02:09 +0100

    fix: various deprecations and bump deps

--- api/migrations/Version20240307125735.php
+++ api/migrations/Version20240307125735.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210930074739 extends AbstractMigration
+final class Version20240307125735 extends AbstractMigration
 {
     public function getDescription(): string
     {
